f853cda5f29aed25107fabf4fa2ee9a665ad251b
e-kreten <ekreten@outlook.com>
2025-05-16 22:14:41 -1100

    Update and rename omni_Armor_X7_Pro.mk to twrp_Armor_X7_Pro.mk

--- twrp_Armor_X7_Pro.mk
+++ twrp_Armor_X7_Pro.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Armor_X7_Pro device
 $(call inherit-product, device/ulefone/Armor_X7_Pro/device.mk)
 
 PRODUCT_DEVICE := Armor_X7_Pro
-PRODUCT_NAME := omni_Armor_X7_Pro
+PRODUCT_NAME := twrp_Armor_X7_Pro
 PRODUCT_BRAND := Ulefone
 PRODUCT_MODEL := Armor X7 Pro
 PRODUCT_MANUFACTURER := ulefone
@@ -24,6 +24,6 @@ PRODUCT_MANUFACTURER := ulefone
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="omni_Armor_X7_Pro-eng 20.1.0 QQ3A.200805.001 eng.lopest.20210331.164908 test-keys"
+    PRIVATE_BUILD_DESC="twrp_Armor_X7_Pro-eng 20.1.0 QQ3A.200805.001 eng.micr0softstore.20210331.164908 test-keys"
 
-BUILD_FINGERPRINT := Ulefone/omni_Armor_X7_Pro/Armor_X7_Pro:20.1.0/QQ3A.200805.001/lopestom03311642:eng/test-keys
+BUILD_FINGERPRINT := Ulefone/twrp_Armor_X7_Pro/Armor_X7_Pro:20.1.0/QQ3A.200805.001/micr0softstore03311642:eng/test-keys
